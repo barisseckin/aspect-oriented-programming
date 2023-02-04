f15485079e40a0734880cd5afc53d9f424102e19
barisseckin <barisseckin0@gmail.com>
2023-02-05 02:02:02 +0300

    after

--- src/main/java/com/springboot/aop/aspects/ServiceAspect.java
+++ src/main/java/com/springboot/aop/aspects/ServiceAspect.java
@@ -1,6 +1,7 @@
 package com.springboot.aop.aspects;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -9,13 +10,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class ServiceAspect {
 
-    @Before("execution(* com.springboot.aop.service.*.*(..))")
+    @Before("execution(* com.springboot.aop.service.*.getMessage(..))")
     public void beforeGetMessage(JoinPoint joinPoint) {
         System.out.println("hi aspect");
         System.out.println(joinPoint.getArgs()[0]);
     }
 
-    public void afterGetMessage() {
-
+    @After("execution(* com.springboot.aop.service.*.getMessage(..))")
+    public void afterGetMessage(JoinPoint joinPoint) {
+        System.out.println("Aspect After: " + joinPoint.getArgs()[0]);
     }
 }
